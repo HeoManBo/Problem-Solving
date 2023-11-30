56d440aa729da7f193087d3118b4af2f0c459227
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2023-11-30 17:19:55 +0900

    [Silver III] Title: 주유소, Time: 96 ms, Memory: 3756 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -3,8 +3,8 @@
 using namespace std;
 
 int N;
-int gas[111'111];
-int length[111'111];
+long long gas[111'111];
+long long length[111'111];
 long long ans = 0;
 
 int main(void)
@@ -20,13 +20,14 @@ int main(void)
 
 	ans = gas[0] * length[1]; //0번째 주유소에서 1번째까지 필요한 비용
 	long long min_gas = gas[0];
+
 	for (int i = 1; i < N-1; i++) {
 		if (min_gas > gas[i]) { //이전에 넣었던 주유소보다 더 싸다면 
 			min_gas = gas[i];
-			ans += (long long)min_gas * length[i+1];
+			ans += (min_gas * length[i+1]);
 		}
 		else { //이전에 넣었던 주유소가 더 싸다면 더 넣음
-			ans += min_gas * length[i+1];
+			ans += (min_gas * length[i+1]);
 		}
 	}
 	cout << ans << '\n';
