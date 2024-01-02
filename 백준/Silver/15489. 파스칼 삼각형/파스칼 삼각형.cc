51f825ff71dca03803ce7fd72ffc39daace83baa
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-01-02 14:34:03 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.cc
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.cc
@@ -1,27 +1,29 @@
 #include<bits/stdc++.h>
-
 using namespace std;
 
-int R, C, W;
-int dp[33][33];
-int main(void)
-{
+int arr[33][33];
+
+int main(void) {
+	int R,C,W;
 	cin >> R >> C >> W;
-	dp[1][1] = 1;
-	for (int i = 1; i <= 30; i++) {
+	arr[1][1] = 1;
+	
+	for (int i = 2; i <= 30; i++) {
 		for (int j = 1; j <= i; j++) {
-			if (i == 1 || j == 1) dp[i][j] = 1;
-			else dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-		}
-	}
-	int sum = 0;
-	for (int i = 0; i < W; i++) {
-		for (int j = 0; j <= i; j++) {
-			sum += dp[R + i][C + j];
+			arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
 		}
 	}
 
-	cout << sum << '\n';
+	int w = 0;
+	int ans = 0;
+	for (int r = R; r < R + W; r++) {
+		for (int j = C; j <= C+w; j++) {
+			ans += arr[r][j];
+		}
+		w++;
+	}
+
+	cout << ans << '\n';
 
 	return 0;
 }
