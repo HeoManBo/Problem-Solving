6c772b47b4f8594d40269c617d66aa85f3385f9c
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-01-10 15:42:59 +0900

    [level 2] Title: H-Index, Time: 0.38 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,18 +1,16 @@
 import java.util.Arrays;
 
 class Solution {
-    public int solution(int[] citations) {
+    public int solution(int[] citations){
         Arrays.sort(citations);
-
+        
         for(int h = citations.length; h >= 1; h--){
-            int count = 0;
-            for(int citation : citations){
-                if(citation >= h) count++;
+            int index = citations.length - h;
+            if(citations[index] >= h) {
+                return h;
             }
-            if(count >= h) return h;
         }
-
-
+        
         return 0;
     }
 }
