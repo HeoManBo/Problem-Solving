6c37195f0bed103b2182bee864d9426638b15cce
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-01-17 23:08:54 +0900

    [level 2] Title: 타겟 넘버, Time: 4.25 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 class Solution {
     private int ans = 0;
     private int target;
@@ -6,9 +8,9 @@ class Solution {
     public int solution(int[] numbers, int target) {
         this.numbers = numbers;
         this.target = target;
-        go(0, 0);
+       //go(0, 0);
         
-        return ans;
+        return usingStack();
     }
 
     private void go(int idx, int sum) {
@@ -19,5 +21,33 @@ class Solution {
         
         go(idx+1, sum - numbers[idx]);
         go(idx + 1, sum + numbers[idx]);
+    }
+    
+    private static class Node{
+         int index;
+         int sum;
+
+        public Node(int index, int sum) {
+            this.index = index;
+            this.sum = sum;
+        }
+    }
+    
+    private int usingStack(){
+        int answer = 0;
+        Stack<Node> st = new Stack<>();
+        st.push(new Node(0, 0));
+        while(!st.empty()){
+            Node node = st.pop();
+            if(node.index == numbers.length){
+                if(node.sum == target) answer++;
+                continue;
+            }
+            
+            st.push(new Node(node.index+1, node.sum + numbers[node.index]));
+            st.push(new Node(node.index+1, node.sum - numbers[node.index]));
+        }
+        
+        return answer;
     }
 }
