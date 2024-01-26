75fc8d6dd40b9a378da0f091ff687a1439da453f
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-01-26 22:08:28 +0900

    [Gold III] Title: 골목 대장 호석 - 효율성 1, Time: 1184 ms, Memory: 20776 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.cc
+++ 백준/Gold/20182. 골목 대장 호석 － 효율성 1/골목 대장 호석 － 효율성 1.cc
@@ -7,7 +7,35 @@ int N, M, A, B, C;
 vector<pair<int, int>> graph[100'001];
 int max_cost[100'001], total_cost[100'001];
 
+bool solve(int MAXMIMUM) { //골목을 지날 때 최대 낼수 있는 돈이 X원일 때 A -> B가 가능할까?
+	int ans = 0x3f3f3f3f;
+	memset(max_cost, 0x3f, sizeof(max_cost));
+	memset(total_cost, 0x3f, sizeof(total_cost));
+	max_cost[A] = 0;
+	total_cost[A] = 0;
 
+	queue<pair<int, pair<int, int>>> q; // {a,{b,c}} a : 토탈 수치심 b : 최대 수치심 c : 현재 위치
+	q.push({ 0, {0,A} });
+
+	while (!q.empty()) {
+		auto x = q.front(); q.pop();
+		int TOTAL = -x.first;
+		int MAX = x.second.first;
+		int now = x.second.second;
+
+		for (pair<int, int> xx : graph[now]) {
+			int next = xx.first;
+			int NEXT_TOTAL = TOTAL + xx.second; //다음 골목을 지날때 최대 수치심
+			if (total_cost[next] > NEXT_TOTAL && NEXT_TOTAL <= C && xx.second <= MAXMIMUM) {
+				max_cost[next] = max(MAX, xx.second);
+				total_cost[next] = NEXT_TOTAL;
+				q.push({ -total_cost[next], {max_cost[next],next } });
+			}
+		}
+	}
+
+	return max_cost[B] == 0x3f3f3f3f ? false : true;
+}
 
 int main(void)
 {
@@ -18,40 +46,15 @@ int main(void)
 		graph[a].push_back({ b,c });
 		graph[b].push_back({ a,c });
 	}
-	
-	memset(max_cost, 0x3f, sizeof(max_cost));
-	memset(total_cost, 0x3f, sizeof(total_cost));
-	max_cost[A] = 0;
-	total_cost[A] = 0;
-	queue<pair<int, pair<int, int>>> q; // {a,{b,c}} a : 토탈 수치심 b : 최대 수치심 c : 현재 위치
-	q.push({ 0, {0,A} });
-	int ans = 0x3f3f3f3f;
-	while (!q.empty()) {
-		auto x = q.front(); q.pop();
-		int TOTAL = -x.first;
-		int MAX = x.second.first;
-		int now = x.second.second;
-		
-		for (pair<int, int> xx : graph[now]) {
-			int next = xx.first;
-	 			int NEXT_TOTAL = TOTAL + xx.second; //다음 골목을 지날때 최대 수치심
-			if (total_cost[next] > NEXT_TOTAL && NEXT_TOTAL <= C) {
-				max_cost[next] = max(MAX, xx.second);
-				total_cost[next] = NEXT_TOTAL;
-				q.push({ -total_cost[next], {max_cost[next],next } });
-				if (next == B) {
-					ans = min(ans, max_cost[next]);
-				}
-			}	
+	int ans = -1;
+
+	for (int i = 20; i >= 1; i--) {
+		if (solve(i)) {
+			ans = i;
 		}
 	}
 	
-	if (ans == 0x3f3f3f3f) {
-		cout << "-1\n";
-	}
-	else {
-		cout << ans << '\n';
-	}
+	cout << ans << '\n';
 
 	return 0;
 }
