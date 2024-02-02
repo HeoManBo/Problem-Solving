14c2ea8a9b41387a1d0b1d689460fc0bfe85d0d7
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-02-02 16:13:19 +0900

    [Gold V] Title: 문자열 잘라내기, Time: 48 ms, Memory: 6876 KB -BaekjoonHub

--- 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.cc
+++ 백준/Gold/2866. 문자열 잘라내기/문자열 잘라내기.cc
@@ -4,6 +4,23 @@ using namespace std;
 string s[1010], filp[1010];
 string join[1010];
 int N, M;
+
+
+bool isValid(int x) { //[x, length)구간을 자른 문자열에 중복이 있는지 확인한다.
+	set<string> chk;
+	for (int i = 0; i < M; i++) {
+		string tmp = filp[i].substr(x, filp[i].length());
+		if (chk.find(tmp) != chk.end()) {
+			return false;
+		}
+		else {
+			chk.insert(tmp);
+		}
+	}
+
+	return true;
+}
+
 int main(void) {
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
@@ -16,7 +33,7 @@ int main(void) {
 		}
 	}
 
-	int count = 0; 
+	/*int count = 0; 
 	for (int k = 1; k < N; k++) {
 		set<string> chk;
 		bool flag = true;
@@ -33,7 +50,25 @@ int main(void) {
 		if (!flag) break;
 		else count++;
 	}
-
 	cout << count << '\n';
+	*/
+
+	int l = 0, r = N-1; //자를 문자열 길이
+	int ans = 0;
+	while (l <= r) {
+		int mid = (l + r) / 2;
+
+		bool flag = isValid(mid);
+
+		if (flag) {
+			ans = l;
+			l = mid + 1;
+		}
+		else {
+			r = mid - 1;
+		}
+	}
+
+	cout << l-1 << '\n';
 	return 0;
 }
