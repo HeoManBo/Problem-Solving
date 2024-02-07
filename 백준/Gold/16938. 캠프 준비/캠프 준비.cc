5d855727b243199d09bedded7e9cab96babc27d4
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-02-07 15:59:34 +0900

    [Gold V] Title: 캠프 준비, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.cc
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.cc
@@ -1,59 +1,46 @@
 #include<bits/stdc++.h>
-#include <unordered_map>
 using namespace std;
 
 int N, L, R, X;
-int arr[1515];
-int ans;
+int arr[22], ans, chk[22];
 
-vector<int> l; //문제 list
-bool chk[10101];
+void solve(int idx, int cnt, vector<int>& list) {
+	if (cnt >= 2) {
+		int MIN = 0x3f3f3f3f;
+		int MAX = -0x3f3f3f3f;
+		int sum=0;
 
-
-void print() {
-	//문제 리스트 출력
-	for (int i : l) {
-		cout << i << " ";
-	}
-	cout << "\n";
-}
-
-void solve(int cnt, int idx, int sum) {
-	if (cnt >= 2) { //고른 문제가 2개 이상이라면
-		int min_x = 987654321;
-		int max_x = -987654321;
-		int s = 0;
-
-		for (int i = 0; i < l.size(); i++) {
-			s += l[i];
-			min_x = min(min_x, l[i]);
-			max_x = max(max_x, l[i]);
+		for (int i : list) {
+			MIN = min(MIN, i);
+			MAX = max(MAX, i);
+			sum += i;
 		}
-		if (s >= L && s <= R && max_x - min_x >= X) {
-			//print();
+
+		if (sum >= L && sum <= R && (MAX - MIN) >= X) {
 			ans++;
 		}
-
 	}
 
 	for (int i = idx; i <= N; i++) {
-		if (chk[i]) continue;
-		if (sum + arr[i] <= R) { 
+		if (!chk[i]) {
 			chk[i] = true;
-			l.push_back(arr[i]);
-			solve(cnt + 1, i, sum + arr[i]);
-			l.pop_back();
+			list.push_back(arr[i]);
+			solve(i + 1, cnt + 1, list);
+			list.pop_back();
 			chk[i] = false;
 		}
 	}
+
 }
 
-int main(void) {
+int main(void)
+{
 	cin >> N >> L >> R >> X;
 	for (int i = 1; i <= N; i++) {
 		cin >> arr[i];
 	}
-	solve(0, 1, 0);
-	cout << ans << "\n";
+	vector<int> list;
+	solve(1, 0, list);
+	cout << ans << '\n';
 	return 0;
 }
