f712ea465faec88c29f8b87406ab123cef494409
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-02-28 11:51:01 +0900

    [Silver I] Title: 포도주 시식, Time: 164 ms, Memory: 15720 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -1,3 +1,5 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -15,14 +17,30 @@ public class Main {
         int[][] dp = new int[N+1][3]; //dp[i][0] : i번쨰를 처음으로 마심, dp[i][1] 이전에 한 번 마심, dp[i][2] 이전에 두 번 연속으로 먹음
         Arrays.stream(dp).forEach(d -> Arrays.fill(d, -1));
 
-        for(int i=0; i<N; i++) {
+        arr.add(0);
+        for(int i=1; i<=N; i++) {
             int num = Integer.parseInt(br.readLine());
             arr.add(num);
         }
 
-        System.out.println(solve(N, arr, dp, 0, 0));
+        System.out.println(solve(N, arr));
     }
 
+    // 반복문
+    private static int solve(int N, List<Integer> arr){
+        int[] dp = new int[N+1];
+        dp[0] = 0;
+        dp[1] = arr.get(1);
+        if(N >= 2) dp[2] = arr.get(1) + arr.get(2);
+        for(int i=3; i<=N; i++){
+            // 이번 걸 패스하거나, 지금걸 이전 연속으로 먹거나, 이번 것이 새거거나
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-3] + arr.get(i-1) + arr.get(i), dp[i-2] + arr.get(i)));
+        }
+
+        return dp[N];
+    }
+
+    //재기
     private static int solve(int N, List<Integer> arr, int[][] dp, int now, int cnt) {
         if(now >= N){ //마지막에 도착한 경우
             return 0;
@@ -41,4 +59,6 @@ public class Main {
 
         return dp[now][cnt];
     }
+
+
 }
