b067f97bf75dbc49fbfbdd09ac924565bf4130a2
HeoManBo <93931871+HeoManBo@users.noreply.github.com>
2024-04-20 00:45:59 +0900

    [Gold V] Title: 홀수 홀릭 호석, Time: 160 ms, Memory: 16252 KB, Score: 102 point -BaekjoonHub

--- 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.java
+++ 백준/Gold/20164. 홀수 홀릭 호석/홀수 홀릭 호석.java
@@ -3,19 +3,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-        int[] ans = go(s,0,0);
+        int[] ans = go(s, 0);
 
         System.out.println(ans[1] + " " + ans[0]);
     }
 
-    private static int[] go(String s, int idx, int odd) {
+    private static int[] go(String s, int odd) {
         // 수가 한자리
         if(s.length() == 1){
             if (((s.charAt(0) - '0') % 2) == 1){
@@ -26,6 +24,7 @@ public class Main {
 
         int[] ret = new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE};
         int o = 0;
+
         for (int i = 0; i < s.length(); i++) {
             if (((s.charAt(i) - '0') % 2) == 1) { //현재 수의 홀수 개수
                 o++;
@@ -37,25 +36,21 @@ public class Main {
             int a = s.charAt(0) - '0';
             int b = s.charAt(1) - '0';
             int sum = a + b;
-            int[] tmp = go(String.valueOf(sum), idx, odd + o);
-            ret[0] = Math.max(ret[0], tmp[0]);
-            ret[1] = Math.min(ret[1], tmp[1]);
+            return go(String.valueOf(sum), odd + o);
         }
 
         //수가 세자리면 임의의 위치에서 3자리를 끊음
-        for (int i = 0; i < s.length()-2; i++) {
-            for (int j = i + 1; j < s.length()-1; j++) {
-                for (int k = j + 1; k < s.length(); k++) {
-                    String sub1 = s.substring(i, j);
-                    String sub2 = s.substring(j, k);
-                    String sub3 = s.substring(k);
+        for (int i = 1; i < s.length()-1; i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                    String sub1 = s.substring(0, i);
+                    String sub2 = s.substring(i, j);
+                    String sub3 = s.substring(j);
                     int sum = Integer.parseInt(sub1) + Integer.parseInt(sub2) + Integer.parseInt(sub3);
-                    int[] tmp = go(String.valueOf(sum), idx, odd + o);
+                    int[] tmp = go(String.valueOf(sum), odd + o);
                     ret[0] = Math.max(ret[0], tmp[0]);
                     ret[1] = Math.min(ret[1], tmp[1]);
                 }
             }
-        }
 
         return ret;
     }
